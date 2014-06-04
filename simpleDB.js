0222d7ee5c95cb624801c9f1848d1296b719506f
Rick Larsen <ricklarsen4@yahoo.com>
2014-06-04 13:35:55 -0700

    debugged simpleDB

--- simpleDB.js
+++ simpleDB.js
@@ -1,30 +1,29 @@
 var characterList = [
-    var player = 
+weaponType = [
+    "sword", "club"
+],
+    player = 
     {
         name: "Amazon",
         bio: "I'm a hero",
         weapon: weaponType[0],
         health: 100,
         score: 0
-    };
+    },
 
-    var monster = [
-        var monsterA = {
+    monster = [
+        monsterA = {
             name: "Vampire",
             bio: "I'm a vampire, I hate garlic.",
             level: 6,
             weapon: weaponType[1] 
-        };
+        },
 
-        var monsterB = {
+        monsterB = {
             name: "Ogre",
             bio: "Ogres are like onions",
             level: 7,
             weapon: weaponType[0]
-        };
-    ];
-];
-
-var weaponType = [
-    "sword", "club"
+        }
+    ]
 ]
