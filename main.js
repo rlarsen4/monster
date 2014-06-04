db392513dfc6bc4b8497bc6501adbe5d690ac8b6
Gloria Adams <thetripler@glorias-air.apollosrc.root>
2014-06-04 11:13:55 -0700

    checked player health for 0, added 1 if diceroll was 0

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ var playerWalk = function() {
 
 var playerTurn = function () {
     var playerAttack = Math.floor((Math.random() * 12));
+        if (playerAttack === 0) {
+            playerAttack += 1;
+        }
     console.log("Your roll: " + playerAttack);
     playerAttack += weaponStrength;
     console.log("your attack: " + playerAttack);
@@ -21,13 +24,17 @@ var playerTurn = function () {
     if (playerAttack >= enemyStrength) {
         playerScore += 100;
         console.log("you win.");
-    } else {
+    }
+    else {
         var enemyAttack = Math.floor((Math.random() * 12));
         console.log("you missed");
         console.log("Enemy's attack: " + enemyAttack);
         playerHealth -= enemyAttack;
-    } 
         console.log("your score: " + playerScore + ". Your health: " + playerHealth);
+        if (playerHealth === 0) {
+            console.log("GAME OVER");
+        }
+    }     
 }
 
 $('#turn').on("click", playerWalk);
