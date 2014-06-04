cabd5f6ba57bcf256c47cc8345318b027626a594
Rick Larsen <ricklarsen4@yahoo.com>
2014-06-04 12:31:13 -0700

    added simpleDB.js

---
+++ simpleDB.js
@@ -0,0 +1,30 @@
+var characterList = [
+    var player = 
+    {
+        name: "Amazon",
+        bio: "I'm a hero",
+        weapon: weaponType[0],
+        health: 100,
+        score: 0
+    };
+
+    var monster = [
+        var monsterA = {
+            name: "Vampire",
+            bio: "I'm a vampire, I hate garlic.",
+            level: 6,
+            weapon: weaponType[1] 
+        };
+
+        var monsterB = {
+            name: "Ogre",
+            bio: "Ogres are like onions",
+            level: 7,
+            weapon: weaponType[0]
+        };
+    ];
+];
+
+var weaponType = [
+    "sword", "club"
+]
