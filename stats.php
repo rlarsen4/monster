1a6adff930af4895cc066efcc007399a3cf3e353
Rick Larsen <ricklarsen4@yahoo.com>
2014-06-05 13:52:49 -0700

    added stats.php

---
+++ stats.php
@@ -0,0 +1,12 @@
+<?php  ?>
+<link rel="stylesheet" href="styles.css">
+<div class="stats">
+    <fieldset>
+        <legend>Stats</legend>
+        <div class="playerType">Player: Amazon</div>
+        <div class="playerBio">Bio: She lives to fight.</div>
+        <div class="health">Health: 100</div>
+        <div class="score">Score: 100</div>
+        <div class="currWeapon">Current Weapon: Sword</div>
+    </fieldset>
+</div>
