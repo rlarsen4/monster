ba032f732bd6febcd3c56531138baa20ff138474
Keith Konetzni <kkonetzni@gmail.com>
2014-06-05 15:02:11 -0700

    Game Dialog Page

---
+++ gamedialog.php
@@ -0,0 +1,86 @@
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="keithStyles.css">
+</head>
+<body>
+
+    <div class="main">
+        <div class="dialog">
+            <div class="monsterContainer">
+                <div class="monsterImg">
+                    <img src="image/elf.png" id="elf">
+                    
+                </div>
+                <div class="monsterDesc">
+                TEXT MONSTER DESCRIPTION MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+
+
+                </div>
+            </div>
+            <div class="weaponContainer">
+                <div class="weaponImg">
+                    <img src="image/sword.png" id="sword">
+                    
+                </div>
+                <div class="weaponBlank">TEXT WEAPON BLANK
+
+
+                </div>
+                
+            </div>
+            
+        </div>
+        <div class="scenery">
+            <img src="image/jungle.png" id="jungle">
+        </div>
+        <div class="controller">TEXT CONTROLLER
+            <div class="">
+
+            </div>
+
+
+            <div class="">
+
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
