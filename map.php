1fb1220c22fc1e83cbec937f9564ed138665f1e5
Rick Larsen <ricklarsen4@yahoo.com>
2014-06-05 13:50:56 -0700

    updated map.php

---
+++ map.php
@@ -0,0 +1,39 @@
+<?php  ?>
+<link rel="stylesheet" href="styles.css">
+<div class="map">
+    <div class="row">
+        <div class="tile visitedTile" id="1"></div>
+        <div class="tile visitedTile" id="2"></div>
+        <div class="tile" id="3"></div>
+        <div class="tile" id="4"></div>
+        <div class="tile" id="5"></div>
+    </div>
+    <div class="row">
+        <div class="tile" id="6"></div>
+        <div class="tile visitedTile" id="7"></div>
+        <div class="tile" id="8"></div>
+        <div class="tile" id="9"></div>
+        <div class="tile" id="10"></div>
+    </div>
+    <div class="row">
+        <div class="tile" id="11"></div>
+        <div class="tile activeTile" id="12"></div>
+        <div class="tile" id="13"></div>
+        <div class="tile" id="14"></div>
+        <div class="tile" id="15"></div>
+    </div>
+    <div class="row">
+        <div class="tile" id="16"></div>
+        <div class="tile" id="17"></div>
+        <div class="tile" id="18"></div>
+        <div class="tile" id="19"></div>
+        <div class="tile" id="20"></div>
+    </div>
+    <div class="row">
+        <div class="tile" id="21"></div>
+        <div class="tile" id="22"></div>
+        <div class="tile" id="23"></div>
+        <div class="tile" id="24"></div>
+        <div class="tile goalTile" id="25"></div>
+    </div>  
+</div>
