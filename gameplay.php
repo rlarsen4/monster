b1cb715e082304f590a85492262c202da2e39227
Rick Larsen <ricklarsen4@yahoo.com>
2014-06-06 11:21:45 -0700

    completed map.php

--- gameplay.php
+++ gameplay.php
@@ -23,9 +23,9 @@ calls in the other php files to populate the page -->
 <link rel="stylesheet" href="styles.css">
 <header> <?php include 'header.php'; ?></header>
 <div>
-<div class="map"><?php include 'map.php' ?> </div>
-<div class="gamedialog"><?php include 'gamedialog.php' ?> 
-<div class="stats"><?php include 'stats.php' ?> </div>
+    <div class="map"><?php include 'map.php' ?> </div>
+    <div class="stats"><?php include 'stats.php' ?> </div>
+    <div class="gamedialog"><?php include 'gamedialog.php' ?> 
 </div>
 </div> 
 
