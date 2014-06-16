191d6e37a101759843ed0aed24051608655105a1
keithk777 <kkonetzni@gmail.com>
2014-06-16 15:49:32 -0700

    changed slightly bc of new dice function

--- monster.js
+++ monster.js
@@ -5,7 +5,7 @@ var Monster = function() {
     this.selectAMonster = function() {
         this.howManyMonsters = characterList[2].length;
         var monsterDice = new Dice(this.howManyMonsters);
-        this.currentMonster = characterList[2][monsterDice.getRoll()];  
+        this.currentMonster = characterList[2][monsterDice.roll()];  
     }
 }
 
