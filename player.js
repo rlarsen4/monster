ba795f1bec99a3bf8022b30705d36e7761f0d09a
keithk777 <kkonetzni@gmail.com>
2014-06-17 16:50:03 -0700

    changed

--- player.js
+++ player.js
@@ -1,89 +1,89 @@
 $(function() {   
 
     var Player = function(selectedPlayer){
-
-        this.currentPlayer = {
-        playerName: [characterList[1][selectedPlayer].name],
-        playerBio: [characterList[1][selectedPlayer].bio],
-        weapon: [characterList[1][selectedPlayer].weapon],
-        health: [characterList[1][selectedPlayer].health],
-        score: [characterList[1][selectedPlayer].score]
-        }
-    console.log (this.currentPlayer); 
-
-    
-    // methods
-    d12 = new Dice(12);
-    d2 = new Dice(2);
-
-    //Player decides to attack
-    this.playerAttack = function () {
         
-        var enemyStrength = 7;
-        console.log(enemyStrength);
+        this.name = [characterList[1][selectedPlayer].name],
+        this.bio = [characterList[1][selectedPlayer].bio],
+        this.weapon = [characterList[1][selectedPlayer].weapon],
+        this.health = [characterList[1][selectedPlayer].health],
+        this.score = [characterList[1][selectedPlayer].score]
+        
+        console.log(this.name);
+        console.log (this.health);
+        console.log(this.score);
 
-        var newAttack = d12.roll();
+
+        // methods
+        d12 = new Dice(12);
+        d2 = new Dice(2);
+
+        //Player decides to attack
+        this.attack = function () {
+            
+            var enemyStrength = 7;
+            console.log(enemyStrength);
+
+            var newAttack = d12.roll();
             if (newAttack === 0) {
                 newAttack += 1;
-                // console.log(newAttack);
+                console.log(newAttack);
             }
-        console.log("Your roll: " + newAttack);
-        
-        if (newAttack >= enemyStrength) {
-            this.score += 100;
-            console.log("you win.");
+            console.log("Your roll: " + newAttack);
+            
+            if (newAttack >= enemyStrength) {
+                aPlayer.score = parseInt(aPlayer.score) + 100;
+                console.log("you win. Your Score: " + aPlayer.score);
+                
+            }
+            else {
+                var enemyAttack = d12.roll();
+                if (enemyAttack) {
+                console.log("you missed");
+                console.log("Enemy's attack: " + enemyAttack);
+                aPlayer.health -= enemyAttack;
+                console.log("your score: " + aPlayer.score + ". Your health: " + aPlayer.health);
+                if (aPlayer.health <= 0) {
+                    aPlayer.health = 0;
+                        console.log("Your Health: " + aPlayer.health);
+                        alert("GAME OVER");
+                    }
+                }     
+            }        
+        }
+
+        // Run Away was pushed
+
+        this.runAway = function() {
+            myRoll = d2.roll();
+            if(myRoll === 0) {
+                aMap.currLocation = aMap.prevLocation;
+                console.log('rolled a zero');
+            }
+            else {
+                aMap.currLocation = aMap.currLocation;
+                console.log('rolled a one');
+
+            }
             
         }
-        else {
-            var enemyAttack = d12.roll();
-            if (enemyAttack) {
-            console.log("you missed");
-            console.log("Enemy's attack: " + enemyAttack);
-            aPlayer.health -= enemyAttack;
-            console.log("your score: " + this.score + ". Your health: " + this.health);
-            if (this.health === 0) {
-                console.log("GAME OVER");
-                }
-            }     
-        }        
-    }
+            
+        // Player can pick up a potion
+        this.pickup = function() {
 
-    // Run Away was pushed
-
-    this.runAway = function() {
-        myRoll = d2.roll();
-        if(myRoll === 0) {
-            // currLocation = prevLocation;
-            console.log('rolled a zero');
-        }
-        else {
-            // currLocation = currLocation;
-            console.log('rolled a one');
-
-        }
+                this.health = this.health + 5;
+            } 
         
-    }
-        
-    // Player can pick up a potion
-    this.playerPickup = function() {
-        if (aWeapon == [characterList[0][0]]) {
-            $('.attack').
-            this.health = this.health + 5;
-        }
-        
-    }
-
-
     
     }
 
-    var aPlayer = new Player(2);
-    console.log (aPlayer.playerName);
-
-    $('.attack').on("click", aPlayer.playerAttack);
+    var aPlayer = new Player(1);
+    console.log (aPlayer.name);
+    
+    $('.attack').on("click", aPlayer.attack);
     $('.runAway').on("click", aPlayer.runAway);
-    $('.pickUp').on("click", aPlayer.playerPickup); 
+    $('.pickUp').on("click", aPlayer.pickup);
+     
 
 
 
-});
+})
