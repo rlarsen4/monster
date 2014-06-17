9330d3609c399c214c85cc6be96db4ac9406fd53
denette <info@mybodyrenew.com>
2014-06-17 16:23:40 -0700

    updated click events for player selector

--- playerSelector.js
+++ playerSelector.js
@@ -1,23 +1,52 @@
 $(function() {
 
-var listPlayers = characterList[1];
-var howManyHeroes = listPlayers.length;
+    var makeHomePage = function(){
+        var listPlayers = characterList[1];
+        var howManyHeroes = listPlayers.length;
 
-for (i = 0; i < howManyHeroes; i++) {
-    $('.playerSelectorFieldset').append("<div class='playerSelectorButton'>" 
-        + "<img src='image/"
-        + listPlayers[i].image
-        + "'><br>"
-        + listPlayers[i].name
-        + "<br>" 
-        + listPlayers[i].bio
-        + "<br>" 
-        + listPlayers[i].weapon);
+        for (i = 0; i < howManyHeroes; i++) {
+            $('.playerSelectorFieldset').append("<div class='playerSelectorButton'>" 
+                + "<img src='image/"
+                + listPlayers[i].image
+                + "'><br>"
+                + listPlayers[i].name
+                + "<br>" 
+                + listPlayers[i].bio
+                + "<br>" 
+                + listPlayers[i].weapon)
+                + '</div>';
+            }
+        $('.playerSelectorFieldset').append("<div class='startGameButton'>Start Game?</div>");
     }
-$('.playerSelectorFieldset').append("<div class='startGameButton'>Start Game?</div>");
 
 
 
+
+
+    var choosePlayer = function(){
+        // change background around the player that was chosen - css()
+        $('.selected').removeClass('selected');
+        $(this).addClass('selected');
+        // Start button appears
+        $('.startGameButton').show();
+        // Clicked on player's bio appears in the Aside under the Directions
+        // $('.bioAside').show();
+        renderBio();
+    };
+
+    var startGame = function(){
+        // Link to and open the gameplay.php
+        window.open('gameplay.php');
+        };
+
+
+    // Event Handlers
+
+    makeHomePage();
+
+    $('.playerSelectorButton').on("click", choosePlayer);
+
+    $('.playerChoices').on("click", '.startButton', startGame);
 
 
 
