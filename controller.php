cc2c93c189579afe7d131ec8735eb5b7a833a4d6
denette <info@mybodyrenew.com>
2014-06-20 14:40:40 -0700

    added styles to buttons on gameplay

--- controller.php
+++ controller.php
@@ -4,21 +4,21 @@
     <div class="directional">
         <div class="row">
             <div class="nothing"></div>
-            <div class="dpad up"><i class="fa fa-arrow-circle-o-up"></i></div>
+            <div class="dpad up button"><i class="fa fa-arrow-circle-o-up"></i></div>
             <div class="nothing"></div>
         </div>
         <div class="row">
-            <div class="dpad left"><i class="fa fa-arrow-circle-o-left"></i></div>
+            <div class="dpad left button"><i class="fa fa-arrow-circle-o-left"></i></div>
             <div class="dpad center"><i class="fa fa-dot-circle-o"></i></div>
-            <div class="dpad right"><i class="fa fa-arrow-circle-o-right"></i></div>
+            <div class="dpad right button"><i class="fa fa-arrow-circle-o-right"></i></div>
         </div>
         <div class="row">
             <div class="nothing"></div>
-            <div class="dpad down"><i class="fa fa-arrow-circle-o-down"></i></div>
-            <div class="nothing"></div>
+            <div class="dpad down button"><i class="fa fa-arrow-circle-o-down"></i></div>
+            <div class="nothing"</div>
         </div>
     </div>
-    <div class="attack">Attack</div>
-    <div class="runAway">Runaway</div>
-    <div class="quit">Quit</div>
+    <div class="attack button">Attack</div>
+    <div class="runAway button">Runaway</div>
+    <div class="quit button">Quit</div>
 </div>
